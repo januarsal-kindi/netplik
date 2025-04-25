cae60bfc42f0925a22ba2746737629b63e670075
Januar Salman Al Kindi <januar@ruangguru.com>
2025-04-25 16:52:38 +0700

    init project

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import LeftSidebar from "@/components/section/LeftSidebar";
+import Navbar from "@/components/section/NavBar";
+import RightSidebar from "@/components/section/RightSidebar";
+import React from "react";
+
+export default function index({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen  2xl:border-x-2 2xl:border-gray-200 dark:2xl:border-zinc-700 ">
+      <LeftSidebar />
+      <main className=" 2xl:max-w-screen-2xl 2xl:mx-auto flex-1 py-10  px-5 sm:px-10 ">
+        <Navbar />
+
+        {children}
+      </main>
+      <RightSidebar />
+    </div>
+  );
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="font-montserrat text-sm bg-white dark:bg-zinc-900 ">
         <Main />
         <NextScript />
       </body>
